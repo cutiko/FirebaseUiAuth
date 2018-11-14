c779d219b78cebdb51ce5b43c29d9c1491cded4b
cutiko <cutiko@gmail.com>
2018-11-14 18:35:07 -0300

    ADD: basic scroll animation

---
+++ app/src/main/java/cl/cutiko/firebaseuiauth/Example.java
@@ -0,0 +1,18 @@
+package cl.cutiko.firebaseuiauth;
+
+import android.os.Handler;
+import android.widget.ScrollView;
+
+public class Example {
+
+    private void something(){
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        ScrollView scrollView;
+        //scrollView.post()
+    }
+}
--- app/src/main/java/cl/cutiko/firebaseuiauth/LoginActivity.kt
+++ app/src/main/java/cl/cutiko/firebaseuiauth/LoginActivity.kt
@@ -1,12 +1,25 @@
 package cl.cutiko.firebaseuiauth
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        /*val animation = ObjectAnimator.ofInt(scrollView, "scrollY", spaceIv.bottom)
+        animation.startDelay = 200
+        animation.setDuration(3200)
+        animation.start()*/
+
+
+        scrollView.post {
+            val animation = ObjectAnimator.ofInt(scrollView, "scrollY",  spaceIv.bottom).setDuration(4200)
+            animation.startDelay = 300
+            animation.start()
+        }
     }
 }
