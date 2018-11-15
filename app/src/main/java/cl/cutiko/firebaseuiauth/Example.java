6b80c039004ec00e4088ba210df4e085f85a7249
cutiko <cutiko@gmail.com>
2018-11-14 23:02:26 -0300

    ADD: tile and animation

--- app/src/main/java/cl/cutiko/firebaseuiauth/Example.java
+++ app/src/main/java/cl/cutiko/firebaseuiauth/Example.java
@@ -1,6 +1,8 @@
 package cl.cutiko.firebaseuiauth;
 
 import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ScrollView;
 
 public class Example {
@@ -14,5 +16,25 @@ public class Example {
         });
         ScrollView scrollView;
         //scrollView.post()
+
+        if (0 == 0) {
+            return;
+        } else {
+
+        }
+
+        scrollView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+        scrollView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/cl/cutiko/firebaseuiauth/LoginActivity.kt
+++ app/src/main/java/cl/cutiko/firebaseuiauth/LoginActivity.kt
@@ -2,6 +2,7 @@ package cl.cutiko.firebaseuiauth
 
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
@@ -12,12 +13,8 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        scrollView.post {
-            val animation = ObjectAnimator.ofInt(scrollView, "scrollY",  spaceIv.bottom).setDuration(4000)
-            animation.startDelay = 300
-            animation.start()
-        }
-
+        writeTitle(0)
+        scrollView.setOnTouchListener { view, event -> true }
         rootVg.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LOW_PROFILE or
                 View.SYSTEM_UI_FLAG_FULLSCREEN or
@@ -25,5 +22,30 @@ class LoginActivity : AppCompatActivity() {
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+
+    }
+
+    fun scrollBackground() {
+        scrollView.post {
+            val animation = ObjectAnimator.ofInt(scrollView, "scrollY",  spaceIv.bottom).setDuration(4000)
+            animation.startDelay = 350
+            animation.start()
+        }
+    }
+
+    fun writeTitle(position: Int) {
+        val text = "Welcome Traveller"
+        Handler().postDelayed({
+            val current = welcomeTv.text
+            val letter = text.get(position)
+            welcomeTv.text = "$current$letter"
+            if (text.length == position + 1) {
+                scrollBackground()
+                welcomeTv.animate().translationY(-500F).setDuration(600).start()
+            } else {
+                writeTitle(position + 1)
+            }
+        }, 250)
     }
 }
