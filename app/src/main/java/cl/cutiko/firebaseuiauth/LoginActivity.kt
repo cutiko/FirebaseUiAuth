98d71a4d00f9df3244bb0756625b1dbfb6aa830d
cutiko <cutiko@gmail.com>
2018-11-14 22:21:20 -0300

    ADD: full screen

--- app/src/main/java/cl/cutiko/firebaseuiauth/LoginActivity.kt
+++ app/src/main/java/cl/cutiko/firebaseuiauth/LoginActivity.kt
@@ -3,6 +3,7 @@ package cl.cutiko.firebaseuiauth
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,9 +13,17 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         scrollView.post {
-            val animation = ObjectAnimator.ofInt(scrollView, "scrollY",  spaceIv.bottom).setDuration(4200)
+            val animation = ObjectAnimator.ofInt(scrollView, "scrollY",  spaceIv.bottom).setDuration(4000)
             animation.startDelay = 300
             animation.start()
         }
+
+        rootVg.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
     }
 }
