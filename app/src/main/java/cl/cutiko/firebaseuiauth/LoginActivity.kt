4eaa1fb33a0771be142fa04dd8a69aacb8b149fe
cutiko <cutiko@gmail.com>
2018-11-14 22:15:01 -0300

    REMOVE: comments

--- app/src/main/java/cl/cutiko/firebaseuiauth/LoginActivity.kt
+++ app/src/main/java/cl/cutiko/firebaseuiauth/LoginActivity.kt
@@ -10,11 +10,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        /*val animation = ObjectAnimator.ofInt(scrollView, "scrollY", spaceIv.bottom)
-        animation.startDelay = 200
-        animation.setDuration(3200)
-        animation.start()*/
-
 
         scrollView.post {
             val animation = ObjectAnimator.ofInt(scrollView, "scrollY",  spaceIv.bottom).setDuration(4200)
