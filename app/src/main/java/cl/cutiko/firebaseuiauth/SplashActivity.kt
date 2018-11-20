3ae92fcc710f2c0d6c94cdd0e87964789a84f4cc
cutiko <cutiko@gmail.com>
2018-11-20 18:42:54 -0300

    ADD: first clue

--- app/src/main/java/cl/cutiko/firebaseuiauth/SplashActivity.kt
+++ app/src/main/java/cl/cutiko/firebaseuiauth/SplashActivity.kt
@@ -30,11 +30,34 @@ class SplashActivity : AppCompatActivity() {
                 .setAvailableProviders(providers)
                 .setAuthMethodPickerLayout(layout)
                 .setTheme(R.style.AppTheme_NoActionBar)
-                .setAlwaysShowSignInMethodScreen(true)
-                .setIsSmartLockEnabled(false)
                 .build(),
             RCSIGNIN)
-
-
     }
 }
+
+/*
+* LOGCAT
+* 2018-11-20 18:42:02.737 9409-9409/cl.cutiko.firebaseuiauth E/AuthUI: A sign-in error occurred.
+    com.firebase.ui.auth.FirebaseUiException: Error when saving credential.
+        at com.firebase.ui.auth.viewmodel.smartlock.SmartLockHandler$1.onComplete(SmartLockHandler.java:98)
+        at com.google.android.gms.tasks.zzj.run(Unknown Source)
+        at android.os.Handler.handleCallback(Handler.java:751)
+        at android.os.Handler.dispatchMessage(Handler.java:95)
+        at android.os.Looper.loop(Looper.java:154)
+        at android.app.ActivityThread.main(ActivityThread.java:6119)
+        at java.lang.reflect.Method.invoke(Native Method)
+        at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:886)
+        at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:776)
+     Caused by: com.google.android.gms.common.api.ApiException: 8:
+        at com.google.android.gms.common.internal.ApiExceptionUtil.fromStatus(Unknown Source)
+        at com.google.android.gms.common.internal.zai.zaf(Unknown Source)
+        at com.google.android.gms.common.internal.zaj.onComplete(Unknown Source)
+        at com.google.android.gms.common.api.internal.BasePendingResult.zaa(Unknown Source)
+        at com.google.android.gms.common.api.internal.BasePendingResult.setResult(Unknown Source)
+        at com.google.android.gms.common.api.internal.BaseImplementation$ApiMethodImpl.setResult(Unknown Source)
+        at com.google.android.gms.internal.auth-api.zzo.zzc(Unknown Source)
+        at com.google.android.gms.internal.auth-api.zzv.dispatchTransaction(Unknown Source)
+        at com.google.android.gms.internal.auth-api.zzd.onTransact(Unknown Source)
+        at android.os.Binder.execTransact(Binder.java:565)
+
+* */
