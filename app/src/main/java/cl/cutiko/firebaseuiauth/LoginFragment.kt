51b324ff9c498a509bcb6733d926cf846f9490b8
cutiko <cutiko@gmail.com>
2018-11-20 17:30:54 -0300

    ADD: basic usage of controller fragment

--- app/src/main/java/cl/cutiko/firebaseuiauth/LoginFragment.kt
+++ app/src/main/java/cl/cutiko/firebaseuiauth/LoginFragment.kt
@@ -13,7 +13,6 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
@@ -22,5 +21,16 @@ class LoginFragment : Fragment() {
         view.visibility = View.GONE
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val rootView = activity?.findViewById<ViewGroup>(R.id.rootVg)
+        rootView?.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
 
 }
