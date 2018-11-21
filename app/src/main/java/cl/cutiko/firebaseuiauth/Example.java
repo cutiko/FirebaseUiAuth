062e2410b478295eb25bc52bd413f6a3e89ee61a
cutiko <cutiko@gmail.com>
2018-11-21 17:43:46 -0300

    ADD: first demo

--- app/src/main/java/cl/cutiko/firebaseuiauth/Example.java
+++ app/src/main/java/cl/cutiko/firebaseuiauth/Example.java
@@ -1,9 +1,12 @@
 package cl.cutiko.firebaseuiauth;
 
+import android.animation.Animator;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ScrollView;
+
+import java.lang.reflect.Field;
 
 public class Example {
 
@@ -36,5 +39,43 @@ public class Example {
             }
         });
 
+        scrollView.performClick();
+
+        try {
+            Field field = View.class.getField(" mOnClickListener");
+            View.OnClickListener clickListener = (View.OnClickListener) field.get(scrollView);
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            };
+
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        scrollView.animate().setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
     }
 }
