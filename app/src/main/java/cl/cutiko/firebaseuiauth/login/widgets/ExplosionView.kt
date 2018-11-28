a1637a97113a7368b6dab87e700abea806e9efa4
cutiko <cutiko@gmail.com>
2018-11-27 21:40:56 -0300

    ADD: explosion is enforce by setting the animation

--- app/src/main/java/cl/cutiko/firebaseuiauth/login/widgets/ExplosionView.kt
+++ app/src/main/java/cl/cutiko/firebaseuiauth/login/widgets/ExplosionView.kt
@@ -2,14 +2,23 @@ package cl.cutiko.firebaseuiauth.login.widgets
 
 import android.content.Context
 import android.graphics.drawable.AnimationDrawable
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageView
+import cl.cutiko.firebaseuiauth.R
 
-class ExplosionView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+class ExplosionView(context: Context?, attrs: AttributeSet?) : ImageView(context, attrs) {
+
+    init {
+        setImageDrawable(
+            ContextCompat.getDrawable(context!!, R.drawable.animation_explosion)
+        )
+    }
 
     fun explode() {
         visibility = View.VISIBLE
-        val animation : AnimationDrawable = background as AnimationDrawable
+        val animation : AnimationDrawable = drawable as AnimationDrawable
         animation.start()
     }
 
