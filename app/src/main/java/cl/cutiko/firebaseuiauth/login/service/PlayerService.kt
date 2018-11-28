cf79d5fb0d467b838c4f758217426f830a31c3a1
cutiko <cutiko@gmail.com>
2018-11-28 19:27:07 -0300

    ADD: basic service

---
+++ app/src/main/java/cl/cutiko/firebaseuiauth/login/service/PlayerService.kt
@@ -0,0 +1,31 @@
+package cl.cutiko.firebaseuiauth.login.service
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.IBinder
+import cl.cutiko.firebaseuiauth.R
+
+
+class PlayerService : Service() {
+
+    private val binder = LocalBinder()
+    private lateinit var mediaPlayer : MediaPlayer
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+
+    fun playMusic(context: Context) {
+        mediaPlayer = MediaPlayer.create(context, R.raw.menu_audio)
+        mediaPlayer.start()
+    }
+
+    class LocalBinder : Binder() {
+        fun getPlayerService() : PlayerService {
+            return PlayerService()
+        }
+    }
+}
