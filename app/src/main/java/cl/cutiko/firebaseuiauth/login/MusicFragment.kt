4f5a33d346382b23d7b9eb10d40243ac8156e5bc
cutiko <cutiko@gmail.com>
2018-11-27 21:58:34 -0300

    ADD: fragment for controlling music

---
+++ app/src/main/java/cl/cutiko/firebaseuiauth/login/MusicFragment.kt
@@ -0,0 +1,19 @@
+package cl.cutiko.firebaseuiauth.login
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import cl.cutiko.firebaseuiauth.R
+
+class MusicFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_music, container, false)
+    }
+
+
+}
