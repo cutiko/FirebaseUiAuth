82c797a09caa458d503178e169dca04cc41cee52
cutiko <cutiko@gmail.com>
2018-12-01 20:00:02 -0300

    ADD: basic dance layout

---
+++ dance/src/main/java/cl/cutiko/dance/BottomFragment.kt
@@ -0,0 +1,17 @@
+package cl.cutiko.dance
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BottomFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_bottom, container, false)
+    }
+
+
+}
---
+++ dance/src/main/java/cl/cutiko/dance/DanceActivity.kt
@@ -0,0 +1,61 @@
+package cl.cutiko.dance
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_dance.view.*
+import kotlinx.android.synthetic.main.layout_auth_dance.*
+
+class DanceActivity : AppCompatActivity() {
+
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_auth_dance)
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+        danceVp.adapter = mSectionsPagerAdapter
+
+    }
+
+
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            return PlaceholderFragment.newInstance(position + 1)
+        }
+
+        override fun getCount(): Int {
+            return 3
+        }
+    }
+
+
+    class PlaceholderFragment : Fragment() {
+
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val rootView = inflater.inflate(R.layout.fragment_dance, container, false)
+            rootView.section_label.text = getString(R.string.section_format, arguments?.getInt(ARG_SECTION_NUMBER))
+            return rootView
+        }
+
+        companion object {
+            private val ARG_SECTION_NUMBER = "section_number"
+            fun newInstance(sectionNumber: Int): PlaceholderFragment {
+                val fragment = PlaceholderFragment()
+                val args = Bundle()
+                args.putInt(ARG_SECTION_NUMBER, sectionNumber)
+                fragment.arguments = args
+                return fragment
+            }
+        }
+    }
+}
