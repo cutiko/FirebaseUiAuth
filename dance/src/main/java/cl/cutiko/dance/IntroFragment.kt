cab08c7c0086541a5bcef36174d48735e7389063
cutiko <cutiko@gmail.com>
2018-12-02 18:45:55 -0300

    ADD: intro fragment to inherit later

---
+++ dance/src/main/java/cl/cutiko/dance/IntroFragment.kt
@@ -0,0 +1,35 @@
+package cl.cutiko.dance
+
+import android.os.Bundle
+import android.support.annotation.DrawableRes
+import android.support.annotation.StringRes
+import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_intro.*
+
+abstract class IntroFragment : Fragment() {
+
+    @get:StringRes
+    protected abstract val getText: Int
+
+    @get:DrawableRes
+    protected abstract val getUndraw : Int
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_intro, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        descriptionTv.setText(getText)
+        if (context != null) {
+            val drawable = ContextCompat.getDrawable(context!!, getUndraw)
+            unDrawIv.setImageDrawable(drawable)
+        }
+
+    }
+
+}
