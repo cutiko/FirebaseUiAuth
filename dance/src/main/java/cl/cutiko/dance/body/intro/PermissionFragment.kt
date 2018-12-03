85cf5776c91ca5fe32398f34f2026d747c10d299
cutiko <cutiko@gmail.com>
2018-12-02 23:09:04 -0300

    ADD: layout is added dynamically

--- dance/src/main/java/cl/cutiko/dance/body/intro/PermissionFragment.kt
+++ dance/src/main/java/cl/cutiko/dance/body/intro/PermissionFragment.kt
@@ -1,5 +1,9 @@
 package cl.cutiko.dance.body.intro
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import cl.cutiko.dance.R
 
 class PermissionFragment : IntroFragment() {
@@ -8,6 +12,11 @@ class PermissionFragment : IntroFragment() {
         fun newInstance() : PermissionFragment {
             return PermissionFragment()
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        LayoutInflater.from(view.context).inflate(R.layout.content_fragment_permission, view as ViewGroup, true)
     }
 
     override val getText: Int
