6b0f0e6376ff5e034301ef21e978afe787ea1022
cutiko <cutiko@gmail.com>
2018-12-02 21:37:39 -0300

    ADD: page change listener

--- dance/src/main/java/cl/cutiko/dance/bottom/BottomFragment.kt
+++ dance/src/main/java/cl/cutiko/dance/bottom/BottomFragment.kt
@@ -3,16 +3,30 @@ package cl.cutiko.dance.bottom
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cl.cutiko.dance.R
 
-class BottomFragment : Fragment() {
+class BottomFragment : Fragment(), ViewPager.OnPageChangeListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_bottom, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity!!.findViewById<ViewPager>(R.id.pagerFragment).addOnPageChangeListener(this)
+    }
+
+    override fun onPageScrollStateChanged(p0: Int) {}
+
+    override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {}
+
+    override fun onPageSelected(page: Int) {
+        Log.d("PAGER", "$page")
+    }
 
 }
