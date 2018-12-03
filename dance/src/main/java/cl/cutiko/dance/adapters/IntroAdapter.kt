75203fd71742a6665680e8fd165e651ef0efd3c2
cutiko <cutiko@gmail.com>
2018-12-02 21:34:29 -0300

    ADD: constants for know slides quantity

--- dance/src/main/java/cl/cutiko/dance/adapters/IntroAdapter.kt
+++ dance/src/main/java/cl/cutiko/dance/adapters/IntroAdapter.kt
@@ -7,6 +7,8 @@ import cl.cutiko.dance.body.intro.MusicFragment
 import cl.cutiko.dance.body.intro.PartyFragment
 import cl.cutiko.dance.body.intro.PermissionFragment
 
+const val INTRO_SLIDES = 3
+
 class IntroAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
@@ -18,6 +20,6 @@ class IntroAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     }
 
     override fun getCount(): Int {
-        return 3
+        return INTRO_SLIDES + 1
     }
 }
