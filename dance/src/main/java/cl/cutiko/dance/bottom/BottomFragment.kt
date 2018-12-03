521bc3852ebe9d09abc0b6408383915308309768
cutiko <cutiko@gmail.com>
2018-12-02 21:46:35 -0300

    ADD: hide and show

--- dance/src/main/java/cl/cutiko/dance/bottom/BottomFragment.kt
+++ dance/src/main/java/cl/cutiko/dance/bottom/BottomFragment.kt
@@ -4,13 +4,19 @@ package cl.cutiko.dance.bottom
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.view.ViewPager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cl.cutiko.dance.R
+import cl.cutiko.dance.adapters.INTRO_SLIDES
 
 class BottomFragment : Fragment(), ViewPager.OnPageChangeListener {
+
+    companion object {
+        private const val FADE_IN = 1F
+        private const val FADE_OUT = 0F
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_bottom, container, false)
@@ -26,7 +32,18 @@ class BottomFragment : Fragment(), ViewPager.OnPageChangeListener {
     override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {}
 
     override fun onPageSelected(page: Int) {
-        Log.d("PAGER", "$page")
+        if (INTRO_SLIDES == page) {
+            fade(FADE_OUT)
+            return
+        }
+        if (view?.alpha != FADE_IN) {
+            fade(FADE_IN)
+        }
+    }
+
+
+    private fun fade(alpha : Float) {
+        view?.animate()?.alpha(alpha)?.setDuration(200)?.start()
     }
 
 }
