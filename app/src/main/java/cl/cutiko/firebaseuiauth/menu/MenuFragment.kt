12116ade62b6a770d50922266d04a8e21338f764
cutiko <cutiko@gmail.com>
2018-12-28 22:37:27 -0300

    ADD: auto signout

--- app/src/main/java/cl/cutiko/firebaseuiauth/menu/MenuFragment.kt
+++ app/src/main/java/cl/cutiko/firebaseuiauth/menu/MenuFragment.kt
@@ -1,11 +1,14 @@
 package cl.cutiko.firebaseuiauth.menu
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cl.cutiko.firebaseuiauth.R
+import com.google.firebase.auth.FirebaseAuth
 
 class MenuFragment : Fragment() {
 
@@ -13,7 +16,8 @@ class MenuFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_menu, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (RESULT_OK == resultCode) FirebaseAuth.getInstance().signOut()
     }
 }
